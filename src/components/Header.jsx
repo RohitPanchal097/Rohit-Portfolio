c04e34a3ca129bc5ebc236e295017c6051d94655
RohitPanchal097 <rohitpancha.8535@gmail.com>
2025-09-12 11:55:59 +0530

    make responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { delay, motion } from "framer-motion";
+import {FiGithub, FiLinkedin, FiMenu, FiX} from "react-icons/fi"
+
+
 
 function Header() {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleMenu = () =>{
+        setIsOpen(!isOpen);
+    }
   return (
     <header className="absolute w-full z-50 transition-all duration-300">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between h-16 md:h-20">
@@ -50,10 +57,84 @@ function Header() {
         </nav>
           {/* Social-icons -Dektop */}
           <div className="md:flex hidden items-center space-x-4 ">
-          <a href="#"></a>
-Roh
+          <motion.a 
+          initial={{opacity:0, x:100}}
+            animate={{opacity:1, x:0}}
+            transition={{
+                type:"spring",
+                stiffness:100,
+                damping:25,
+                delay:1.2,
+                duration:1.2,
+            }}
+          
+          className="text-gray-800 dark:text-gray-200 hover:text-violet-600 dark:hover:text-violet-400 transition-colors duration-300 " href="#"  >
+           <FiGithub className="w-5 h-5 "/>
+          
+          </motion.a>
+           <motion.a 
+          initial={{opacity:0, x:100}}
+            animate={{opacity:1, x:0}}
+            transition={{
+                type:"spring",
+                stiffness:100,
+                damping:25,
+                delay:1.2,
+                duration:1.2,
+            }}
+          
+          className="text-gray-800 dark:text-gray-200 hover:text-violet-600 dark:hover:text-violet-400 transition-colors duration-300 " href="#"  >
+          <FiLinkedin className="w-5 h-5 "/>
+          
+          </motion.a>
+         
           </div>
+
+          {/* Hire Me Button */}
+            <motion.button
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+                delay: 1.5,
+                duration: 0.8,
+                type: "spring",
+                stiffness: 100,
+                damping: 15,
+            }}
+            className= "ml-4 px-4 py-2 rounded-xl bg-gradient-to-r from-gray-400 to-gray-100 text-violet-700 font-bold hover:from-violet-600 hover:to-violet-400 hover:text-white transition-all duration-300 hidden md:inline-block">
+        Hire Me
+            </motion.button>
+
+            {/* Mobile Menu Icon */}
+            <div className="md:hidden flex items-center ">
+                <motion.button 
+                onClick={toggleMenu}
+                whileTap={{scale:0.8}}
+                className="text-gray-300">
+                    {isOpen ? <FiX className = "h-6 w-6" /> : <FiMenu className = "h-6 w-6" />}
+                </motion.button>
+
+            </div>
+          
       </div>
+        {/* Mobile Menu */}
+        <motion.div
+        initial={{height:0, opacity:0}}
+        animate ={{
+            opacity: isOpen ? 1: 0,
+            height: isOpen ? "auto" : 0,
+        }}
+        transition={{duration:0.3,}}
+        className="md:hidden overflow-hidden bg-white dark:bg-gray-800 shadow-lg px-4 py-5 space-y-5">
+            <nav className="flex flex-col space-y-4">
+            {["Home", "About", "Projects", "Contact"].map((item, index) => (
+                <a onClick={toggleMenu} key={item} href="#" className="text-gray-300 font-medium py-2">
+                    {item}
+                </a>
+            ))}
+            </nav>
+            
+        </motion.div>
     </header>
   );
 }
